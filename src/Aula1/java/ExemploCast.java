9638f4c08b297d72d284181f843cc138585f14c4
nataliarosa88 <nataliag1988@gmail.com>
2020-01-11 21:20:05 -0300

    Aula 1

---
+++ src/Aula1/java/ExemploCast.java
@@ -0,0 +1,10 @@
+public class ExemploCast {
+	public static void main (String args[]) {
+		
+		double valor = 10000F;
+		float valor2 = (float) 10000D;
+		System.out.println(valor);
+		System.out.println(valor2);
+	
+	}
+}
